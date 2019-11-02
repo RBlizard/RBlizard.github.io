d8ac933250c96aa9044769f4fa0f39ae981305ce
RBlizard <RBlizard@users.noreply.github.com>
2019-11-01 17:51:47 -0700

    Update sketch.js
    Changing sketch to only draw when mouse is pressed, added a wiping button, and changing color/opacity while mouse is continuously pressed

--- Miscelaneous Projects/javaScript tests/sketch.js
+++ Miscelaneous Projects/javaScript tests/sketch.js
@@ -1,14 +1,27 @@
+let value = 255
+
 function setup() {
-createCanvas(800,600);
-background(80);
+  createCanvas(800,600);
+  background(80);
 }
 
 function draw() {
+  
+  //draws an ellipse and changes blueness and opacity while mouse is pressed (blue is unchanged)
   if (mouseIsPressed) {
-    fill(0);
+    fill(value, value, 255, (value*2));
     ellipse(mouseX, mouseY, 80, 80);
-    } else {
-    fill(255);
-    }
-    
+    value = value - 1;
+  } 
+}
+
+
+
+
+//Wipes the canvas and resets draw color when space bar is pressed
+function keyTyped() {
+  if (key === ' '){
+    background(80);
+    value = 255;
   }
+}
